621abf3769482bd2337871aed5948dc4ac9304ac
keya singh <mail2keyasingh@gmail.com>
2020-12-07 19:49:01 +0530

    i learn't constrained bodies.

---
+++ Chain.js
@@ -0,0 +1,21 @@
+class Chain {
+    constructor(bodya,bodyb){
+        var options ={
+            bodyA: bodya,
+            bodyB: bodyb,
+            length: 10,
+            stiffness: 0.04
+        }
+      
+        this.chain=Constraint.create(options);
+        World.add(world, this.chain);
+     
+    }
+
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(5);
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,14 +39,18 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(200,200,20,PI);
+
+    chain= new Chain(bird.body,log6.body);
+
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+   // console.log(box2.body.position.y);
+  //  console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -58,6 +65,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    log6.display();
+    chain.display();
 
     bird.display();
     platform.display();
